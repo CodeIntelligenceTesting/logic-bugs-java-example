12d2776761f82916eb52042b96a1348db1eb25a9
Josh Grant <joshua.m.grant@gmail.com>
2023-04-05 10:15:08 -0400

    assertion-based test

---
+++ src/test/java/TriangleInequalityTest.java
@@ -0,0 +1,21 @@
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import com.code_intelligence.jazzer.junit.FuzzTest;
+import org.junit.jupiter.api.Assertions;
+
+/**
+ * Example of a logical test verifying the <a href="https://en.wikipedia.org/wiki/Triangle_inequality">Triangle inequality</a>
+ * using generated values from the Fuzzed Data provider
+ */
+public class TriangleInequalityTest {
+
+    @FuzzTest
+    public void verifyInequality(FuzzedDataProvider data){
+        Double x = 1.0;
+        Double y = data.consumeDouble();
+
+        Double leftSide = Math.abs(x - y);
+        Double rightSide = Math.abs(x) + Math.abs(y);
+
+        Assertions.assertTrue(leftSide <= rightSide);
+    }
+}
